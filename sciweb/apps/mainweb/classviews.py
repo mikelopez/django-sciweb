8f9b140bda4110b2ef47aef6eb23311f91dee0f8
Marcos Lopez <dev@scidentify.info>
2013-07-13 13:57:19 -0400

    websites model classviews

--- sciweb/apps/mainweb/classviews.py
+++ sciweb/apps/mainweb/classviews.py
@@ -7,9 +7,28 @@ except ImportError:
     LOG_ON = False
 from lib.mainlogger import LoggerLog
 
-#from models import Gallery, Category
+from models import Website
 #from forms import *
 
 class AdminIndexView(TemplateView):
     """ Index Page View """
     template_name = "mainweb/admin-index.html"
+
+class WebsiteView(ListView):
+    """ Website List Page View """
+    model = Website
+
+class CreateWebsite(CreateView):
+    """ Create Website page view """
+    model = Website
+
+class UpdateWebsite(UpdateView):
+    """ Update view """
+    model = Website
+    
+class WebsiteDetailView(DetailView):
+    """ Website Detail Page View """
+    queryset = Website.objects.all()
+    def get_object(self, **kwargs):
+        object = super(WebsiteDetailView, self).get_object(**kwargs)
+        return object
--- sciweb/apps/mainweb/urls.py
+++ sciweb/apps/mainweb/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
-from mainweb.views import AdminIndexView
+from mainweb.views import AdminIndexView, \
+        CreateWebsite, UpdateWebsite, WebsiteView, WebsiteDetailView
         
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,14 +19,12 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', AdminIndexView.as_view(), name="admin-view"),
+    url(r'^$', AdminIndexView.as_view(), name="adminview"),
 
-    #url(r'^dynamic/(?P<model_name>.*)/add', AddSomething.as_view(), name="dynamic-crud"),
-
-    #url(r'^gallery/add', CreateGallery.as_view(), name="galleries-add"),
-    #url(r'^gallery/update', UpdateGallery.as_view(), name="galleries-update"),
-    #url(r'^gallery/', GalleryView.as_view(), name="galleries-view"),
-    #url(r'^galleries/(?P<pk>\d+)/$', GalleryDetailView.as_view(), name="gallery-detail"),
+    url(r'^website/add', CreateWebsite.as_view(), name="website_add"),
+    url(r'^website/update', UpdateWebsite.as_view(), name="website_update"),
+    url(r'^website/', WebsiteView.as_view(), name="website_view"),
+    url(r'^website/(?P<pk>\d+)/$', WebsiteDetailView.as_view(), name="website_detail"),
     
 
     #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (PROJECT_ROOTDIR), 'show_indexes': True}),
