fd82b320a18e28526bab3613a4add4dc34223339
Mike Lopez <dev@scidentify.info>
2013-07-23 04:31:33 -0400

    added urls for website pages crud

--- sciweb/apps/mainweb/urls.py
+++ sciweb/apps/mainweb/urls.py
@@ -25,6 +25,11 @@ urlpatterns = patterns('',
     url(r'^website/update', UpdateWebsite.as_view(), name="website-update"),
     url(r'^website/(?P<pk>\d+)', WebsiteDetailView.as_view(), name="website-detail"),
     url(r'^website/', WebsiteView.as_view(), name="website-view"),
+
+    url(r'^websitepage/add', CreateWebsitePage.as_view(), name="websitepage-add"),
+    url(r'^websitepage/update', UpdateWebsitePage.as_view(), name="websitepage-update"),
+    url(r'^websitepage/(?P<pk>\d+)', WebsitePageDetailView.as_view(), name="websitepage-detail"),
+    url(r'^websitepage/', WebsitePageView.as_view(), name="websitepage-view"),
     
 
     #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (PROJECT_ROOTDIR), 'show_indexes': True}),
