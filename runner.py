22e3b93b879dbfc4ffe160dd7ea91ffba8d95c62
Mike Lopez <dev@scidentify.info>
2013-10-08 02:43:29 -0400

    modified runner

--- runner.py
+++ runner.py
@@ -4,23 +4,21 @@ from django.conf import settings
 PROJECT_ROOTDIR = os.path.realpath(os.path.dirname(__file__))
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
-sys.path.insert(0, os.path.join(PROJECT_PATH, ''))
-sys.path.insert(1, os.path.join(PROJECT_PATH, 'apps'))
 settings.configure(DEBUG=True,
                   DATABASES={
                         'default': {
                             'ENGINE': 'django.db.backends.sqlite3',
                         }
                   },
-                  ROOT_URLCONF='sciweb.apps.mainweb.urls',
+                  ROOT_URLCONF='mainweb.urls',
                   INSTALLED_APPS=('django.contrib.auth',
                                   'django.contrib.contenttypes',
                                   'django.contrib.sessions',
                                   'django.contrib.admin',
-                                  'sciweb.apps.mainweb',))
+                                  'mainweb',))
 
 from django.test.simple import DjangoTestSuiteRunner
 test_runner = DjangoTestSuiteRunner(verbosity=1)
-failures = test_runner.run_tests(['sciweb.apps.mainweb', ])
+failures = test_runner.run_tests(['mainweb', ])
 if failures:
     sys.exit(failures)
