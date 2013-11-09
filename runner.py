92005e50e831a84d6e43f93b7e543bb84524d25b
Mike Lopez <dev@scidentify.info>
2013-11-09 03:05:30 -0500

    runner adjustment

--- runner.py
+++ runner.py
@@ -3,11 +3,15 @@ from django.conf import settings
 
 PROJECT_ROOTDIR = os.path.realpath(os.path.dirname(__file__))
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(PROJECT_PATH, ''))
 
 settings.configure(DEBUG=True,
+                  PROJECT_ROOTDIR=PROJECT_ROOTDIR,
+                  PROJECT_PATH=PROJECT_PATH,
                   DATABASES={
                         'default': {
                             'ENGINE': 'django.db.backends.sqlite3',
+                            'NAME': 'dbtestdata.sqlite',
                         }
                   },
                   ROOT_URLCONF='mainweb.urls',
@@ -15,10 +19,10 @@ settings.configure(DEBUG=True,
                                   'django.contrib.contenttypes',
                                   'django.contrib.sessions',
                                   'django.contrib.admin',
-                                  'mainweb',))
+                                  'mainweb','xxxgalleries',))
 
 from django.test.simple import DjangoTestSuiteRunner
 test_runner = DjangoTestSuiteRunner(verbosity=1)
-failures = test_runner.run_tests(['mainweb', ])
+failures = test_runner.run_tests(['mainweb', 'xxxgalleries',])
 if failures:
     sys.exit(failures)
