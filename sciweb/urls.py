a6d974c1e8dffbc83db764b442db94dda25bd1a3
Mike Lopez <dev@scidentify.info>
2013-12-22 05:01:12 -0500

    urls fix

--- sciweb/urls.py
+++ sciweb/urls.py
@@ -1,21 +1,22 @@
 from django.conf.urls.defaults import *
-from settings import PROJECT_ROOTDIR
 # Uncomment the next two lines to enable the admin:
+from django.conf import settings
+from django.contrib.auth.views import login, logout
 from django.contrib import admin
 admin.autodiscover()
 
-from settings import ENABLE_ADMIN, PROJECT_ROOTDIR, TEMPLATE_PATH
-from django.contrib.auth.views import login, logout
+PR = getattr(settings, "PROJECT_ROOTDIR", None)
+TP = getattr(settings, "TEMPLATE_PATH", None)
+ENABLE_ADMIN = getattr(settings, "ENABLE_ADMIN", None)
 
 urlpatterns = patterns('',
-    (r'src/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (PROJECT_ROOTDIR), 'show_indexes': True}),
-    (r'rel/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (TEMPLATE_PATH), 'show_indexes': True}),
-    (r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/media/' % (PROJECT_ROOTDIR), 'show_indexes': True}),
-    (r'assets/ico/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (TEMPLATE_PATH), 'show_indexes': True}),
+    (r'src/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (PR), 'show_indexes': True}),
+    (r'rel/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (TP), 'show_indexes': True}),
+    (r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/media/' % (PR), 'show_indexes': True}),
+    (r'assets/ico/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (TP), 'show_indexes': True}),
     (r'logout', logout),
     (r'accounts/login/$', login),
     (r'login/$', login),
-
 
     # defaults
     url(r'^robots.txt', 'mainweb.views.robots', name="mainweb_robots"),
@@ -23,10 +24,26 @@ urlpatterns = patterns('',
     (r'^(?P<linkname>[-\w]+)/(?P<filtername>[\w -]+)', 'mainweb.views.index'),
     (r'^(?P<linkname>[-\w]+)','mainweb.views.index'),
 )
+
 if ENABLE_ADMIN:
-    urlpatterns += patterns('', 
-    # app urls
+    urlpatterns = patterns('',
+        (r'src/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (PR), 'show_indexes': True}),
+        (r'rel/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (TP), 'show_indexes': True}),
+        (r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/media/' % (PR), 'show_indexes': True}),
+        (r'assets/ico/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'%s/static/' % (TP), 'show_indexes': True}),
+        (r'logout', logout),
+        (r'accounts/login/$', login),
+        (r'login/$', login),
+
+        # app urls
         (r'mainweb/', include('mainweb.urls')),
-        (r'xxxgalleries/', include('xxxgalleries.urls')),)
+        (r'xxxgalleries/', include('xxxgalleries.urls')),
+
+        # defaults
+        url(r'^robots.txt', 'mainweb.views.robots', name="mainweb_robots"),
+        url(r'^$', 'mainweb.views.index', name="mainweb_index"),
+        (r'^(?P<linkname>[-\w]+)/(?P<filtername>[\w -]+)', 'mainweb.views.index'),
+        (r'^(?P<linkname>[-\w]+)','mainweb.views.index'),
+    )
 
 
