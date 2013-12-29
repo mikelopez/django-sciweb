1a61ac4d1821aa26845135e9b0e4accba430ae32
Mike Lopez <dev@scidentify.info>
2013-12-29 02:15:18 -0500

    remove urls from settings and fix import error for >= 1.6

--- sciweb/urls.py
+++ sciweb/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import *
+try:
+    # <= 1.5
+    from django.conf.urls.defaults import *
+except:
+    # >= 1.6
+    from django.conf.urls import *
 # Uncomment the next two lines to enable the admin:
 from django.conf import settings
 from django.contrib.auth.views import login, logout
